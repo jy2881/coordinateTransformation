baef7ef48febdb8fc4826e824dc3e221a5b8e99b
panjw@esrichina.com.cn <panjw@esrichina.com.cn>
2018-01-31 14:46:09 +0800

    aa

--- test2.py
+++ test2.py
@@ -5,9 +5,11 @@ import arcpy
 import numpy as np
 import test
 
+# 两个数据，a是EStonia1992坐标系的要素类，b是他转为wgs84坐标系后的数据要素类
 a = r"D:\workspace\python\coordinate\data.gdb\Estonia"
 b = r"D:\workspace\python\coordinate\data.gdb\Estonia_Project"
 
+# 用来存储ax,ay,az,bx,by,bz
 vector = [[],[],[],[],[],[]]
 
 a_Estonia = 6378137
@@ -27,3 +29,25 @@ with arcpy.da.SearchCursor(b,["POINT_X","POINT_Y"]) as cursor:
         for i in range(3,6):
             vector[i].append(result[i-3])
 
+for i in range(0, len(vector[0])):
+    if i == 0:
+        B = np.array([[1, 0, 0, vector[0][i], 0, vector[2][i], -1*vector[1][i]],
+                          [0, 1, 0, vector[1][i], -1*vector[2][i], 0, vector[0][i]],
+                          [0, 0, 1, vector[2][i], vector[1][i], -1*vector[0][i], 0]])
+        l = np.array([[vector[0][i]-vector[3][i]],
+                      [vector[1][i]-vector[4][i]],
+                      [vector[2][i]-vector[5][i]]])
+    else:
+        B = np.vstack((B, np.array([[1, 0, 0, vector[0][i], 0, vector[2][i], -1*vector[1][i]],
+                          [0, 1, 0, vector[1][i], -1*vector[2][i], 0, vector[0][i]],
+                          [0, 0, 1, vector[2][i], vector[1][i], -1*vector[0][i], 0]])))
+        l = np.vstack((l, np.array([[vector[0][i]-vector[3][i]],
+                                    [vector[1][i]-vector[4][i]],
+                                    [vector[2][i]-vector[5][i]]])))
+
+"""
+求解七参数，文档中给的七参数应该是：
+dx: 0.055, dy: -0.541, dz: -0.185, ds: -0.014， rx: -0.0183, ry: 0.0003, rz: 0.007
+"""
+x = np.dot(np.dot(np.linalg.inv(np.dot(B.T, B)), B.T), l) * np.array([[1], [1], [1], [1e6], [3600], [3600], [3600]])
+print(x)
